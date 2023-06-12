e7cd6d565321837603d3edbfe17f9fc79f30afa3
ZacharyGo <ZSGoEmpire@gmail.com>
2023-06-12 17:45:37 +0800

    02 - MVC Basics - CRUD Operation/012 Create Category View

---
+++ BulkyBook/BulkyBookWeb/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
