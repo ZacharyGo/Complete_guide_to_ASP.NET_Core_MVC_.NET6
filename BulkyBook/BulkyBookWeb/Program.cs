149a480d9e26858771c3f238f9d1653f66879776
ZacharyGo <ZSGoEmpire@gmail.com>
2023-06-12 18:19:43 +0800

    08 - Identity Management/001 Scaffold Identity

--- BulkyBook/BulkyBookWeb/Program.cs
+++ BulkyBook/BulkyBookWeb/Program.cs
@@ -3,6 +3,7 @@ using BulkyBook.DataAccess.Repository;
 using BulkyBook.DataAccess.Repository.IRepository;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDBContext>();
 /*builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();*/
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
